14e5fe61e9e2055e699449ef25bef400de219740
RubioFM <rubiofranciscom14@gmail.com>
2018-03-26 11:10:27 -0300

    arreglos en el login y vista login

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -25,7 +25,7 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/empresa';
+    protected $redirectTo = '/gasto';
 
     /**
      * Create a new controller instance.
